0cd95efbfe69b9a6f1e6dadff859a7f7f785bcd0
Mohmmad Shiplak <Mohmmad2Shiplak@gmail.com>
2024-09-29 02:40:28 +0300

    Add New Features Successfully

--- Bank Project/Bank_DataAccess/clsDataAccessSettings.cs
+++ Bank Project/Bank_DataAccess/clsDataAccessSettings.cs
@@ -1,22 +1,20 @@
-﻿//using Microsoft.Extensions.Configuration;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
-using Microsoft.Extensions.Configuration;
+
+
 
 namespace Bank_DataAccess
 {
     public static   class clsDataAccessSettings
     {
-        public static  string Connectionstring= "Server=.;database=Bank Project ;User Id=sa;Password=sa123456";
+        public static string Connectionstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+           // "Server=.;database=Bank Project ;User Id=sa;Password=sa123456";
 
          
-
-
-
-
     }
 }
